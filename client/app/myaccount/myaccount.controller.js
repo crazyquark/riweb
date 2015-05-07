552fa9d25fd65ec8f7833dabb4746466edbd692f
Andrei Cristian Petcu <andrei.petcu@cegeka.com>
2015-05-07 17:37:37 +0300

    first connection to a ripple server

--- client/app/myaccount/myaccount.controller.js
+++ client/app/myaccount/myaccount.controller.js
@@ -2,9 +2,30 @@
 
 angular.module('riwebApp')
   .controller('MyaccountCtrl', function ($scope, Auth) {
-    $scope.message = 'Hello';
     $scope.getMyAccountUser = Auth.getCurrentUser;
     $scope.createWallet = function () {
         swal("Good job!", "You created an new wallet!", "success")
     };
+
+    $scope.message = 'Not connected to any server';
+
+    var Remote = ripple.Remote;
+    var remote = new Remote({
+        // see the API Reference for available options
+        servers: [ 'wss://s1.ripple.com:443' ]
+//        servers: [ 'wss://localhost:6006' ]
+    });
+
+    remote.connect(function() {
+        /* remote connected */
+        remote.requestServerInfo(function(err, info) {
+            if(info.info.pubkey_node){
+                $scope.message = 'Connected to server ' + info.info.pubkey_node;
+            } else {
+                $scope.message = 'Error ' + err;
+            }
+            $scope.$apply();
+
+        });
+    });
   });
