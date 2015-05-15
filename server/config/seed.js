68f7348921f70972affd19ba0cccb6e7bb378b21
Andrei Cristian Petcu <andrei.petcu@cegeka.com>
2015-05-15 13:43:27 +0300

    cleanup wallets on restart

--- server/config/seed.js
+++ server/config/seed.js
@@ -7,6 +7,7 @@
 
 var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
+var Wallet = require('../api/wallet/wallet.model');
 
 Thing.find({}).remove(function() {
   Thing.create({
@@ -47,3 +48,5 @@ User.find({}).remove(function() {
     }
   );
 });
+
+Wallet.find({}).remove().exec();
