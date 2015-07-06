af285f12309b72f231484369bd19fc4607e47d25
Andrei Cristian Petcu <andrei.petcu@cegeka.com>
2015-07-06 17:43:52 +0300

    merged if (currentUser.role == 'admin') {

--- client/app/RippleWalletService/RippleWalletService.service.js
+++ client/app/RippleWalletService/RippleWalletService.service.js
@@ -71,7 +71,7 @@ angular.module('riwebApp')
                             if (data.length < 1) {
                                 var newWallet = {};
                                 newWallet.ownerEmail = currentUser.email;
-                                if (currentUser.email === 'admin@admin.com') {
+                                if (currentUser.role == 'admin') {
                                     var checkColdWalletFlags = function () {
                                         RippleRemoteService.onRemotePresent(function (remote) {
 
