63c6c2f0a2293d2c31a7e5af432ed2c6ba65b17e
crazyquark <sam.bitcoin@gmail.com>
2015-07-07 16:30:07 +0300

    Renamed trustLineService to trustLineService

---
+++ client/app/TrustLineService/TrustLineService.service.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('riwebApp')
+  .service('TrustLineService', function (Auth, RIPPLE_ROOT_ACCOUNT) {
+
+        function makeInitialTrustLines(walletInfo, remote, rippleAddress) {
+            remote.setSecret(walletInfo.wallet.publicKey, walletInfo.wallet.passphrase);
+
+            /*jshint camelcase: false */
+            var transaction = remote.createTransaction('TrustSet', {
+                account: rippleAddress,
+                limit: '10000/EUR/' + RIPPLE_ROOT_ACCOUNT.address,
+                set_flag: 'NoRipple'
+            });
+
+            transaction.submit(function (err, res) {
+                if (err) {
+                    swal('Error', 'Sorry there was a problem processing your request! ' + err.message, 'error');
+                }
+                if (res) {
+                    swal('Congratulations, ' + Auth.getCurrentUser().name + '!', 'You just got a new wallet! ' + rippleAddress, 'success');
+                }
+            });
+        }
+
+        function buildMakeInitialTrustLines(walletInfo, remote){
+            return function(rippleAddress){
+                makeInitialTrustLines(walletInfo, remote, rippleAddress);
+            };
+        }
+
+        return {
+            buildMakeInitialTrustLines: buildMakeInitialTrustLines
+        };
+  });
