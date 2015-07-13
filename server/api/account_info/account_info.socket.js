5be3fc99a44762a7de411d9f94c51f8f91d94cf4
crazyquark <sam.bitcoin@gmail.com>
2015-07-13 17:56:01 +0300

    More stubby stubs

--- server/api/account_info/account_info.socket.js
+++ server/api/account_info/account_info.socket.js
@@ -4,6 +4,24 @@
 
 'use strict';
 
-exports.register = function(socket) {
+var Wallet = require('./../wallet/wallet.model');
 
+function get_account_info(owner_email, socket) {
+  Wallet.find({ownerEmail: owner_email}, function(err, wallets) {
+      if (wallets && wallets.length == 1) { // There should be only one
+
+      }
+      else {
+
+      }
+  });
+
+}
+
+exports.get_account_info = get_account_info;
+
+exports.register = function(socket) {
+  socket.on('account_info', function(owner_email) {
+
+  });
 }
