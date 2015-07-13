16a55e23f0f2929537195a1fe9ac48a41e7fabec
crazyquark <sam.bitcoin@gmail.com>
2015-07-13 17:38:28 +0300

    rly remove controller

--- server/api/account_info/index.js
+++ server/api/account_info/index.js
@@ -1,10 +1,5 @@
 'use strict';
 
 var express = require('express');
-var controller = require('./account_info.controller');
-
 var router = express.Router();
-
-router.get('/', controller.index);
-
 module.exports = router;
