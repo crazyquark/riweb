f4f80513bb3692aed81a37cd6b9f45be9e8ba1f1
Andrei Cristian Petcu <andrei.petcu@cegeka.com>
2015-07-13 17:55:59 +0300

    created utils.js, made riweb tests work with rippled started

--- server/api/create_wallet/create_wallet.spec.js
+++ server/api/create_wallet/create_wallet.spec.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var should = require('should');
 var sinon = require('sinon');
 var app = require('../../app');
 var create_wallet = require('./create_wallet.socket');
@@ -8,6 +7,7 @@ var chai = require('chai');
 var io = require('socket.io');
 var expect = chai.expect;
 var ripple = require('ripple-lib');
+var Utils = require('./../../core/utils');
 
 var nonAdminGeneratedWallet = {
     address: 'rNON_ADMIN4rj91VRWn96DkukG4bwdtyTh',
@@ -15,24 +15,26 @@ var nonAdminGeneratedWallet = {
 };
 
 describe('Test create wallet', function() {
-    var socket;
+    var socket, remote;
     beforeEach(function() {
         socket = {};
         socket.emit = sinon.spy();
         socket.on = sinon.spy();
 
-        var nonAdminCallback = sinon.stub().returns(nonAdminGeneratedWallet);
-        ripple.Wallet.generate = nonAdminCallback;
+        ripple.Wallet.generate = sinon.stub().returns(nonAdminGeneratedWallet);
+
+        remote = {
+            connect: sinon.spy(),
+            setSecret: sinon.spy()
+        };
+        Utils.getNewRemote = sinon.stub().returns(remote);
 
         create_wallet.register(socket);
     });
 
     it('should create root wallet for admin@admin.com', function(done) {
         create_wallet.create_wallet_for_email('admin@admin.com').then(function(newWallet){
-//            console.log('before');
             expect(newWallet.publicKey).to.equal('rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh');
-//            expect(newWallet.publicKey).to.equal('rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh ');
-//            console.log('after');
             done();
         });
     });
