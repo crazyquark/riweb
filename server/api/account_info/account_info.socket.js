20668fe491755471795dba20ddc1797de0097929
crazyquark <sam.bitcoin@gmail.com>
2015-07-14 10:30:45 +0300

    Working towerds account_info stuff

--- server/api/account_info/account_info.socket.js
+++ server/api/account_info/account_info.socket.js
@@ -4,12 +4,37 @@
 
 'use strict';
 
+var ripple = require('ripple-lib');
+var Utils = require('./../../core/utils');
 var Wallet = require('./../wallet/wallet.model');
+
+function get_ripple_account_info(ripple_address, callback) {
+  var remote = Utils.getNewRemote();
+
+  var options = {
+    account: ripple_address,
+    ledger: 'validated'
+  };
+
+  remote.requestAccountInfo(options, function(err, info) {
+      var request = remote.requestAccountInfo(options, function(err, info) {
+          if (!err) {
+            callback(info);
+          }
+        });
+  });
+}
 
 function get_account_info(owner_email, socket) {
   Wallet.find({ownerEmail: owner_email}, function(err, wallets) {
-      if (wallets && wallets.length == 1) { // There should be only one
+      if (wallets && wallets.length === 1) { // There should be only one
+        var wallet = wallets[0];
 
+        get_ripple_account_info(wallet.publicKey, function(err, account_info) {
+            var account = {
+              balance: account_info.Balance
+            }
+        });
       }
       else {
 
@@ -18,10 +43,12 @@ function get_account_info(owner_email, socket) {
 
 }
 
+
+
 exports.get_account_info = get_account_info;
 
 exports.register = function(socket) {
   socket.on('account_info', function(owner_email) {
-
+    get_account_info(owner_email, socket);
   });
 }
