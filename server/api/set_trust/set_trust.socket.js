d0347a3efc18af6b9de99eb697814205212e7db1
crazyquark <sam.bitcoin@gmail.com>
2015-07-16 15:07:07 +0300

    Does this work? (set_trust)

--- server/api/set_trust/set_trust.socket.js
+++ server/api/set_trust/set_trust.socket.js
@@ -4,18 +4,42 @@
 
 'use strict';
 
+var Q = require('q');
+
 var Utils = require('./../../utils/utils');
 
 /**
 * Creates a trst line between 2 ripple accounts
-* @param rippleDestination target trustline endpoint
-* @param rippleSource source trustline endpoint
+* @param rippleDestinationAddr target trustline endpoint
+* @param rippleSourceAddr source trustline endpoint
 * @param rippleSourceSecret source address secret (used to create the trustline)
 */
-function set_trust(rippleDestination, rippleSource, rippleSourceSecret) {
-  var remote = Utils.getNewConnectedRemote(rippleSource, );
+function set_trust(rippleDestinationAddr, rippleSourceAddr, rippleSourceSecret, limit, currency) {
+  var currency = currency || 'EUR';
+  var limit = limit || 1000;
 
+  var deferred = Q.defer();
 
+  var remote = Utils.getNewConnectedRemote(rippleSourceAddr, rippleSourceSecret);
+
+  var transaction = remote.createTransaction('TrustSet', {
+    account: rippleSourceAddr,
+    limit: limit + '/' + currency + + '/' + rippleDestinationAddr;
+  });
+
+  transaction.on('reusbmitted', function()) {
+    // mkay...
+  }
+
+  transaction.submit(function(err, res) {
+    if(!err) {
+      deferred.resolve(null); // See no evil
+    } else {
+      deferred.reject(err);
+    }
+  });
+
+  return deffered.promise;
 }
 
 exports.set_trust = set_trust;
