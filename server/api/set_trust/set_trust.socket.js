15a4dedfc97ed1a80b5443155da5b79720bf9212
Andrei Cristian Petcu <andrei.petcu@cegeka.com>
2015-07-16 15:33:15 +0300

    small fixes

--- server/api/set_trust/set_trust.socket.js
+++ server/api/set_trust/set_trust.socket.js
@@ -13,23 +13,26 @@ var Utils = require('./../../utils/utils');
 * @param rippleDestinationAddr target trustline endpoint
 * @param rippleSourceAddr source trustline endpoint
 * @param rippleSourceSecret source address secret (used to create the trustline)
+* @param limit the limit of the thrustline (defaults to 1000)
+* @param currency the currency of the thrustline (defaults to 'EUR')
 */
 function set_trust(rippleDestinationAddr, rippleSourceAddr, rippleSourceSecret, limit, currency) {
-  var currency = currency || 'EUR';
-  var limit = limit || 1000;
+  limit = limit || 1000;
+  currency = currency || 'EUR';
 
   var deferred = Q.defer();
 
   var remote = Utils.getNewConnectedRemote(rippleSourceAddr, rippleSourceSecret);
 
-  var transaction = remote.createTransaction('TrustSet', {
+    var transaction_limit = limit + '/' + currency +'/' + rippleDestinationAddr;
+    var transaction = remote.createTransaction('TrustSet', {
     account: rippleSourceAddr,
-    limit: limit + '/' + currency + + '/' + rippleDestinationAddr;
+    limit: transaction_limit
   });
 
-  transaction.on('reusbmitted', function()) {
+  transaction.on('reusbmitted', function() {
     // mkay...
-  }
+  });
 
   transaction.submit(function(err, res) {
     if(!err) {
@@ -39,11 +42,11 @@ function set_trust(rippleDestinationAddr, rippleSourceAddr, rippleSourceSecret, 
     }
   });
 
-  return deffered.promise;
+  return deferred.promise;
 }
 
 exports.set_trust = set_trust;
 
 exports.register = function(socket) {
 
-}
+};
