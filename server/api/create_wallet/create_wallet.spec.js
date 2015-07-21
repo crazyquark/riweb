b856ba7cc4feb7616cb65cc6807665ea9967d3ea
Andrei Cristian Petcu <andrei.petcu@cegeka.com>
2015-07-21 11:20:41 +0300

    use camel case CreateWallet

--- server/api/create_wallet/create_wallet.spec.js
+++ server/api/create_wallet/create_wallet.spec.js
@@ -10,7 +10,7 @@ var ripple = require('ripple-lib');
 var sinonChai = require("sinon-chai");
 chai.use(sinonChai);
 
-var create_wallet = require('./create_wallet.socket');
+var CreateWallet = require('./create_wallet.socket');
 var TestingUtils = require('./../../../test/utils/testing_utils');
 var Utils = require('./../../utils/utils');
 var Wallet = require('./../wallet/wallet.model');
@@ -30,7 +30,7 @@ describe('Test create_wallet', function () {
         Utils.getNewConnectedRemote = sinon.stub().returns(Q(remote));
         Utils.getNewConnectedAdminRemote = sinon.stub().returns(Q(remote));
 
-        create_wallet.register(socket);
+        CreateWallet.register(socket);
         emitSpy = sinon.spy(Utils.getEventEmitter(), 'emit');
     });
 
@@ -43,7 +43,7 @@ describe('Test create_wallet', function () {
     });
 
     it('should create root wallet for admin@admin.com', function (done) {
-        create_wallet.createWalletForEmail('admin@admin.com').then(function () {
+        CreateWallet.createWalletForEmail('admin@admin.com').then(function () {
             expect(Wallet.create).to.have.been.calledWith(TestingUtils.getAdminMongooseWallet());
             expect(Wallet.create).to.have.callCount(1);
             done();
@@ -51,7 +51,7 @@ describe('Test create_wallet', function () {
     });
 
     it('should create non-root wallet for a1@example.com', function (done) {
-        create_wallet.createWalletForEmail('a1@example.com').then(function () {
+        CreateWallet.createWalletForEmail('a1@example.com').then(function () {
           expect(Wallet.create).to.have.been.calledWith(TestingUtils.getNonAdminMongooseWallet('a1@example.com'));
           expect(Wallet.create).to.have.callCount(1);
           done();
@@ -59,8 +59,8 @@ describe('Test create_wallet', function () {
     });
 
     it('should not create duplicate wallet for a2@example.com', function (done) {
-        create_wallet.createWalletForEmail('a2@example.com').then(function () {
-            create_wallet.createWalletForEmail('a2@example.com').then(function () {
+        CreateWallet.createWalletForEmail('a2@example.com').then(function () {
+            CreateWallet.createWalletForEmail('a2@example.com').then(function () {
                 expect(Wallet.create).to.have.been.calledWith(TestingUtils.getNonAdminMongooseWallet('a2@example.com'));
                 expect(Wallet.create).to.have.callCount(1);
                 done();
@@ -69,7 +69,7 @@ describe('Test create_wallet', function () {
     });
 
     it('should set_trust when create new wallet', function (done) {
-        create_wallet.createWalletForEmail('a3@example.com').then(function () {
+        CreateWallet.createWalletForEmail('a3@example.com').then(function () {
             expect(emitSpy).to.have.callCount(1);
             expect(emitSpy).to.have.been.calledWith('set_trust', {
                 rippleDestinationAddr: adminMongooseWallet.address,
@@ -81,7 +81,7 @@ describe('Test create_wallet', function () {
     });
 
     it('should set root flag when create new wallet', function (done) {
-        create_wallet.createWalletForEmail('admin@admin.com').then(function () {
+        CreateWallet.createWalletForEmail('admin@admin.com').then(function () {
             expect(emitSpy).to.have.callCount(1);
             expect(emitSpy).to.have.been.calledWith('set_root_flags');
             done();
