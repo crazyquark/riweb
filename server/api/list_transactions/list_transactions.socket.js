bedf2d3d951e57613ea7578cc2a25761ec368cd4
crazyquark <sam.bitcoin@gmail.com>
2015-07-23 17:59:27 +0300

    Attempt to translate transactions to user accounts

--- server/api/list_transactions/list_transactions.socket.js
+++ server/api/list_transactions/list_transactions.socket.js
@@ -6,8 +6,63 @@
 
 var ripple = require('ripple-lib');
 var Q = require('q');
+
 var Utils = require('./../../utils/utils');
 var Wallet = require('./../wallet/wallet.model');
+
+function translateTransactionsToHuman(transactionsList) {
+	var deferred = Q.defer();
+
+	var transactionsListHuman = [];
+	var walletsPromises = [];
+	var result = [];
+	
+	transactionsList.forEach(function (transaction) {
+
+		var transactionHuman = {
+			account: transaction.tx.Destination,
+			fee: transaction.tx.Fee,
+			txType: transaction.tx.TransactionType,
+			date: transaction.tx.date,
+			amount: transaction.tx.Amount // Amount is a number for XRP or an object { currency, issuer, value } for EUR
+		}
+
+		if (transactionHuman.txType == 'Payment') {
+			if (typeof transactionHuman.amount === 'object') {
+				transactionHuman.amount = transactionHuman.amount.value; // Extract only EUR value	
+				transactionsListHuman[transactionHuman.account] = transactionHuman;
+				
+				// Wait for the owner email from the DB
+				walletsPromises.push(Wallet.findByRippleAddress(transactionHuman.account));
+			}
+		}
+
+	});
+
+	Q.allSettled(walletsPromises)
+		.then(function (walletPromisesResults) {
+			walletPromisesResults.forEach(function (walletPromiseResult) {
+				if (walletPromiseResult.state === 'fulfilled') {
+					// If the user no longer exists in the DB the promise will resolve to null
+					if (walletPromiseResult) {
+						var wallet = walletPromiseResult.value;
+				
+						// Replace account with ownerEmail
+						result.push({
+							destination: wallet.ownerEmail,
+							amount: transactionsListHuman[wallet.address].amount,
+							fee: transactionsListHuman[wallet.address].fee
+						});
+					}
+					delete transactionsListHuman[wallet.address]
+				}
+			});
+
+			deferred.resolve(result);
+		});
+
+	return deferred.promise;
+}
 
 function listTransactions(ownerEmail, socket) {
 	var deferred = Q.defer();
@@ -56,9 +111,12 @@ function listTransactions(ownerEmail, socket) {
 					socket.emit('post:list_transactions', result);
 					deferred.resolve(result);
 				} else {
-					result = { status: 'success', transactions: res.transactions };
-					socket.emit('post:list_transactions', result);
-					deferred.resolve(result);
+					translateTransactionsToHuman(res.transactions)
+						.then(function (transactionsHuman) {
+							result = { status: 'success', transactions: transactionsHuman };
+							socket.emit('post:list_transactions', result);
+							deferred.resolve(result);
+						});
 				}
 			});
 		});
