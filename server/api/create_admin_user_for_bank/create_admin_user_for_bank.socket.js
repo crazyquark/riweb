7cc9500528ae80440b80f4a1818c5b5524dcf97a
crazyquark <sam.bitcoin@gmail.com>
2015-07-31 11:45:36 +0300

    stub implementation for createAdminUserForBank

--- server/api/create_admin_user_for_bank/create_admin_user_for_bank.socket.js
+++ server/api/create_admin_user_for_bank/create_admin_user_for_bank.socket.js
@@ -4,21 +4,19 @@
 
 'use strict';
 
-var CreateAdminUserForBank = require('./create_admin_user_for_bank.model');
+var Q = require('q');
+
+function createAdminUserForBank(adminUserInfo) {
+    var deferred = Q.defer();
+    
+    deferred.resolve(null);
+        
+    return deferred.promise;
+}
+
+exports.createAdminUserForBank = createAdminUserForBank;
 
 exports.register = function(socket) {
-  CreateAdminUserForBank.schema.post('save', function (doc) {
-    onSave(socket, doc);
-  });
-  CreateAdminUserForBank.schema.post('remove', function (doc) {
-    onRemove(socket, doc);
-  });
+
 }
 
-function onSave(socket, doc, cb) {
-  socket.emit('create_admin_user_for_bank:save', doc);
-}
-
-function onRemove(socket, doc, cb) {
-  socket.emit('create_admin_user_for_bank:remove', doc);
-}
--- server/api/create_admin_user_for_bank/create_admin_user_for_bank.spec.js
+++ server/api/create_admin_user_for_bank/create_admin_user_for_bank.spec.js
@@ -1,20 +1,30 @@
 'use strict';
 
-var should = require('should');
+var sinon = require('sinon');
 var app = require('../../app');
-var request = require('supertest');
+var Q = require('q');
+var mongoose = require('mongoose');
+var chai = require('chai');
+var expect = chai.expect;
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
 
-describe('GET /api/create_admin_user_for_banks', function() {
+var TestingUtils = require('../../../test/utils/testing_utils');
+var Utils = require('../../utils/utils');
 
-  xit('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/create_admin_user_for_banks')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });
+var CreateAdminUserForBank = require('./create_admin_user_for_bank.socket');
+
+describe('Test create admin user for bank', function () {
+
+  it('should create an user when a create_admin_user_for_bank event is received', function (done) {
+    var adminInfo = {
+      bankId:  mongoose.Types.ObjectId('55bb258310e509b66b6a9c27'),
+      email: 'admin@brd.com',
+      password: 'secret',
+    };
+    
+    CreateAdminUserForBank.createAdminUserForBank(adminInfo).then(function(user){
+      done();
+    });
   });
 });
