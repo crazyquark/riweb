7c8b268de8c9838b700dbf76f99197584ac19c37
crazyquark <sam.bitcoin@gmail.com>
2015-07-31 14:51:39 +0300

    stub register for bank/client side

--- client/app/createbank/createbank.controller.js
+++ client/app/createbank/createbank.controller.js
@@ -2,5 +2,34 @@
 
 angular.module('riwebApp')
   .controller('CreatebankCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.bank = {};
+
+    $scope.register = function (form) {
+      $scope.submitted = true;
+
+      if (form.$valid) {
+        // Auth.createUser({
+        //   name: $scope.user.name,
+        //   email: $scope.user.email,
+        //   password: $scope.user.password,
+        //   iban: $scope.user.iban,
+        //   bank: $scope.user.bank._id
+        // })
+        //   .then(function () {
+        //     // Account created, redirect to home
+        //     $location.path('/myaccount');
+        //   })
+        //   .catch(function (err) {
+        //     err = err.data;
+        //     $scope.errors = {};
+
+        //     // Update validity of form fields that match the mongoose errors
+        //     angular.forEach(err.errors, function (error, field) {
+        //       form[field].$setValidity('mongoose', false);
+        //       $scope.errors[field] = error.message;
+        //     });
+        //   });
+      }
+    };
   });
+ 
