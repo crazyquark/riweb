03bc30bbc120a66338f3c7aa66e1e08e2ff819ff
Andrei Cristian Petcu <andrei.petcu@cegeka.com>
2015-08-13 09:50:20 +0300

    fixed setBanksTrust which I broke previously

--- server/api/set_trust/set_trust.socket.js
+++ server/api/set_trust/set_trust.socket.js
@@ -10,7 +10,7 @@ var Utils = require('./../../utils/utils');
 
 var SetRootFlags = require('./../set_root_flags/set_root_flags.socket');
 
-var debug = require('debug')('SetTrust');
+var debug = require('debug')('===========SetTrust');
 
 /**
 * Creates a trust line between 2 ripple accounts
@@ -29,12 +29,15 @@ function setTrust(rippleDestinationAddr, rippleSourceAddr, rippleSourceSecret, l
   debug('setTrust: ', rippleDestinationAddr, rippleSourceAddr, rippleSourceSecret, limit, currency);
 
   Utils.getNewConnectedRemote(rippleSourceAddr, rippleSourceSecret).then(function(remote) {
-    var transaction = remote.createTransaction('TrustSet', {
+    debug('getNewConnectedRemote', rippleSourceAddr, rippleSourceSecret);
+    var transactionOptions = {
       account: rippleSourceAddr,
       limit: limit + '/' + currency + '/' + rippleDestinationAddr
-    });
+      };
+      var transaction = remote.createTransaction('TrustSet', transactionOptions);
 
-    transaction.submit(function(err, res) {
+    transaction.submit(function(err) {
+      debug('transaction.submit', transactionOptions, err);
       if(!err) {
         deferred.resolve({status: 'success'}); // See no evil
       } else {
@@ -68,11 +71,12 @@ function setBanksTrust(bank1, bank2, user1, user2) {
   var deferred = Q.defer();
 
   SetRootFlags.setBankFlags(bank1, bank2, user1, user2).then(function () {
+    var user1SetTrustAll = setTrustAll([bank1.address/*, bank2.address*/], user1.address, user1.secret);
+    var user2SetTrustAll = setTrustAll([/*bank1.address,*/ bank2.address], user2.address, user2.secret);
+    var bank2SetTrust = setTrustAll([bank1.address], bank2.address, bank2.secret);
+    var bank1SetTrust = setTrustAll([bank2.address], bank1.address, bank1.secret);
 
-    var user1SetTrustAll = setTrustAll([bank1.address, bank2.address], user1.address, user1.secret);
-    var user2SetTrustAll = setTrustAll([bank1.address, bank2.address], user2.address, user2.secret);
-
-    Q.all(user1SetTrustAll, user2SetTrustAll).then(function(){
+    Q.all(user1SetTrustAll, user2SetTrustAll, bank1SetTrust, bank2SetTrust).then(function(){
       deferred.resolve({ status: 'success' });
     });
   });
