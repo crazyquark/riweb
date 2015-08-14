e3762d0e1c2861ce8b88d8f5f8f281b557f8e288
crazyquark <sam.bitcoin@gmail.com>
2015-08-14 12:03:25 +0300

    create trust between all banks on creation, async

--- server/api/create_bank/create_bank.socket.js
+++ server/api/create_bank/create_bank.socket.js
@@ -10,6 +10,7 @@ var Wallet = require('./../wallet/wallet.model');
 var Utils = require('./../../utils/utils');
 var BankAccount = require('./../bankaccount/bankaccount.model');
 var CreateWallet = require('../create_wallet/create_wallet.socket');
+var SetTrust = require('../set_trust/set_trust.socket');
 
 var debug = require('debug')('CreateBank');
 
@@ -30,7 +31,6 @@ function createNewBank(newBank) {
   return BankAccount.createQ(newBankAccount);
 }
 
-
 function createBank(newBank) {
   var deferred = Q.defer();
 
@@ -42,12 +42,20 @@ function createBank(newBank) {
       debug('createNewBank', newBank);
       createNewBank(newBank).then(function (createdBank) {
         // Need to also fund this newly minted wallet
-        // XXX: need lots of XRP for banks to fund wallets
+        // XXX need lots of XRP for banks to fund wallets
         CreateWallet.fundWallet(createdBank.hotWallet, ROOT_RIPPLE_ACCOUNT, 1000).then(function () {
           debug('funded bank wallet');
           // XXX should the funding be chained or should we return to user immediately? I'd rather not wait for the costly ripple operation
           // Utils.getEventEmitter().emit('post:fund_wallet', createdBank.hotWallet);
           socket.emit('post:fund_wallet', createdBank.hotWallet.address);
+          
+          // Establish trust lines with the other banks. Account must be funded.
+          // XXX this is also made async after returning to the client so not to block the create bank page too long;
+          // The client should listen for the emitted event
+          SetTrust.setMutualBanksTrust(createdBank).then(function () {
+            debug('setMutualBanksTrust done');
+            socket.emit('post:set_mutual_banks_trust', { status: 'success' });
+          });
         });
 
         Utils.getEventEmitter().emit('create_admin_user_for_bank', {
@@ -56,6 +64,7 @@ function createBank(newBank) {
           email: newBank.email,
           password: newBank.password,
         });
+
 
         debug('resolve create', createdBank.info);
         deferred.resolve(createdBank);
