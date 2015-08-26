74fa7f1b543e0a5e95365a504c2ac2ab928d5fe4
crazyquark <sam.bitcoin@gmail.com>
2015-08-26 14:45:05 +0300

    fix IT make_transfer

--- test/integration/make_transfer.spec.js
+++ test/integration/make_transfer.spec.js
@@ -7,6 +7,7 @@ var chai = require('chai');
 var expect = chai.expect;
 
 var TestingUtils = require('../utils/testing_utils');
+
 var MakeTransfer = require('../../server/api/make_transfer/make_transfer.socket');
 var CreateWallet = require('../../server/api/create_wallet/create_wallet.socket')
 var SetTrust = require('../../server/api/set_trust/set_trust.socket');
@@ -15,27 +16,29 @@ var Utils = require('../../server/utils/utils');
 var debug = require('debug')('=======TMakeTransfer');
 
 describe('ITest transfers', function () {
-	var socketSpy, user, bank, userWallet;
+	var socketSpy, emitter, user, bank, userWallet;
 
 	beforeEach(function (done) {
 		this.timeout(50000); // The Talented Mr. Ripple takes a long time to fund wallets
 		socketSpy = TestingUtils.buildSocketSpy();
-		MakeTransfer.register(socketSpy);
+		emitter = TestingUtils.buildNewClientEventEmitterSpy(socketSpy);
+
+		MakeTransfer.register(socketSpy, emitter);
 
 		TestingUtils.dropMongodbDatabase().then(function () {
-			TestingUtils.seedBankAndUser(function (newUser, userBank) {
+			TestingUtils.seedBankAndUser(emitter, function (newUser, userBank) {
 				user = newUser;
 				bank = userBank;
-				CreateWallet.fundWallet(bank.hotWallet, Utils.ROOT_RIPPLE_ACCOUNT).then(function () {
-					CreateWallet.createWalletForEmail(user.email).then(function (wallet) {
+				CreateWallet.fundWallet(emitter, bank.hotWallet, Utils.ROOT_RIPPLE_ACCOUNT).then(function () {
+					CreateWallet.createWalletForEmail(emitter, user.email).then(function (wallet) {
 						userWallet = wallet;
-              SetTrust.setTrust(userBank.hotWallet.address, userWallet.address, userWallet.secret, 1000, 'EUR').then(function () {
+						SetTrust.setTrust(userBank.hotWallet.address, userWallet.address, userWallet.secret, 1000, 'EUR').then(function () {
 							done();
-            }).done(null, function(error){done(error);});
-          }).done(null, function(error){done(error);});
-        }).done(null, function(error){done(error);});
-      });
-    }).done(null, function(error){done(error);});
+						}).done(null, function (error) { done(error); });
+					}).done(null, function (error) { done(error); });
+				}).done(null, function (error) { done(error); });
+			});
+		}).done(null, function (error) { done(error); });
 	});
 
 	afterEach(function () {
@@ -44,7 +47,7 @@ describe('ITest transfers', function () {
 
 	it('Transfer from admin to regular user', function (done) {
 		this.timeout(10000);
-		MakeTransfer.makeTransfer(bank.email, user.email, 10).then(function (result) {
+		MakeTransfer.makeTransfer(emitter, bank.email, user.email, 10).then(function (result) {
 			expect(result.status).to.eql('success');
 			done();
 		}, function (err) {
