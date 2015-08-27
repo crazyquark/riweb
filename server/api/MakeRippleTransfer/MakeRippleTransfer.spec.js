db253cc049ed292ce00588e471a26b51a1c7bfec
Andrei Cristian Petcu <andrei.petcu@cegeka.com>
2015-08-27 15:47:50 +0300

    removed unused test

--- server/api/MakeRippleTransfer/MakeRippleTransfer.spec.js
+++ server/api/MakeRippleTransfer/MakeRippleTransfer.spec.js
@@ -4,17 +4,6 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-xdescribe('GET /api/MakeRippleTransfer', function() {
+describe('GET /api/MakeRippleTransfer', function() {
 
-  it('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/MakeRippleTransfer')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });
-  });
 });
